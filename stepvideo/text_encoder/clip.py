5ef9876a8a5fd450cef0f0ffdb7bf2ab1dc92e40
Vivicai1005 <caiweiwei1005@gmail.com>
2025-04-09 20:34:21 +0800

    loop embedding

--- stepvideo/text_encoder/clip.py
+++ stepvideo/text_encoder/clip.py
@@ -19,17 +19,26 @@ class HunyuanClip(nn.Module):
     @torch.no_grad
     def forward(self, prompts, with_mask=True):
         self.device = next(self.text_encoder.parameters()).device
-        text_inputs = self.tokenizer(
-            prompts,
-            padding="max_length",
-            max_length=self.max_length,
-            truncation=True,
-            return_attention_mask=True,
-            return_tensors="pt",
-        )
-        prompt_embeds = self.text_encoder(
-            text_inputs.input_ids.to(self.device),
-            attention_mask=text_inputs.attention_mask.to(self.device) if with_mask else None,
-        )
-        return prompt_embeds.last_hidden_state, prompt_embeds.pooler_output
+        all_last_hidden_states = []
+        all_pooler_outputs = []
+
+        for prompt in prompts:
+            text_inputs = self.tokenizer(
+                prompt,
+                padding="max_length",
+                max_length=self.max_length,
+                truncation=True,
+                return_attention_mask=True,
+                return_tensors="pt",
+            )
+            outputs = self.text_encoder(input_ids=text_inputs.input_ids.to(self.device),
+                                        attention_mask=text_inputs.attention_mask.to(
+                                            self.device) if with_mask else None, )
+            all_last_hidden_states.append(outputs.last_hidden_state)
+            all_pooler_outputs.append(outputs.pooler_output)
+
+        last_hidden_states = torch.cat(all_last_hidden_states, dim=0)
+        pooler_output = torch.cat(all_pooler_outputs, dim=0)
+
+        return last_hidden_states, pooler_output
         
